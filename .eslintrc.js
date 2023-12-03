7db10621930857e77e7527642dbe1034eb2d8997
Kent Hu <kenthu@kenthu.net>
2023-12-03 10:30:44 -0800

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+  ],
+  rules: {
+    "import/order": [
+      "error",
+      { "newlines-between": "always", alphabetize: { order: "asc" } },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
+};
